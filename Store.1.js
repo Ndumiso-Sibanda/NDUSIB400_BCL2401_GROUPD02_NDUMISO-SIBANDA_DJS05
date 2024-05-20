92bfc25989c92cf1cbbf608d824c1fc0761ddc13
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-05-20 16:47:51 +0200

    Add getState and dispatch methods to Store class: getState returns the current state; dispatch updates the state using the reducer and notifies listeners

---
+++ Store.1.js
@@ -0,0 +1,15 @@
+// store.js
+class Store {
+ constructor(reducer, initialState) {
+  this.reducer = reducer;
+  this.state = initialState;
+  this.listeners = [];
+ }
+ getState() {
+  return this.state;
+ }
+ dispatch(action) {
+  this.state = this.reducer(this.state, action);
+  this.listeners.forEach((listener) => listener());
+ }
+}
--- store.js
+++ store.js
@@ -1,9 +1,1 @@
-// store.js
 
-class Store {
- constructor(reducer, initialState) {
-  this.reducer = reducer;
-  this.state = initialState;
-  this.listeners = [];
- }
-}

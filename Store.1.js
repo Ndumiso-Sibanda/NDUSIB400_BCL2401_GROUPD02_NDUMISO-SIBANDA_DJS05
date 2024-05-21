0c596b3a14f394448179bb1b0dd634670d07acf9
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-05-21 09:28:30 +0200

    Add counterReducer function

--- Store.1.js
+++ Store.1.js
@@ -19,3 +19,17 @@ class Store {
   };
  }
 }
+
+// Reducer function to handle actions
+function counterReducer(state = { count: 0 }, action) {
+ switch (action.type) {
+  case "ADD":
+   return { count: state.count + 1 };
+  case "SUBTRACT":
+   return { count: state.count - 1 };
+  case "RESET":
+   return { count: 0 };
+  default:
+   return state;
+ }
+}
